5bda400680aee2aa0b55216c33619dcda3d01c62
Erick Rost <erickrosts@gmail.com>
2026-02-17 21:13:19 -0300

    add

--- classes/db_class1.sql
+++ classes/db_class1.sql
@@ -8,8 +8,8 @@ data_ocorrencia DATE
 );
 
 INSERT INTO focos_calor (estado, bioma, data_ocorrencia) VALUES
-('Amazonas', 'Amazônia', '2025-02-01’),
-('Mato Grosso', 'Cerrado', '2025-02-03’),
+('Amazonas', 'Amazônia', '2025-02-01'),
+('Mato Grosso', 'Cerrado', '2025-02-03'),
 ('Pará', 'Amazônia', '2025-02-05');
 
 SELECT * FROM focos_calor;
